af0a4d5bce62253cd3e1e104b4dadd7c658d3943
ardotheedu <eduardosalima3@gmail.com>
2021-08-06 21:41:57 -0300

    feat: Header

---
+++ src/pages/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import styles from './dashboard.module.css'
-import Carousel from 'react-bootstrap/Carousel'
+import styles from './dashboard.module.scss'
 import { api } from '../services/api';
 
 export default function Dashboard() {
