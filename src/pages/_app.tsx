f315b40c7881a86b3108df6c024fecd71ffbd9de
ardotheedu <eduardosalima3@gmail.com>
2021-09-19 01:12:05 -0300

    feat: react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from 'next/app'
 import '../styles/global.scss';
 import { AuthProvider } from '../contexts/ArtistsContext';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { queryClient } from '../services/queryClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
-    </>
+      
+      {/* <ReactQueryDevtools /> */}
+    </QueryClientProvider>
   )
 }
 
